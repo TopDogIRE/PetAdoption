324aad7f215d5faca2a31dd9bff7914e96d489ef
Jflands23 <75229373+Jflands23@users.noreply.github.com>
2021-05-18 17:48:45 -0700

    added some basic layouts. a log in scree and a sign up screen

--- app/src/main/java/com/example/petadoption/MainActivity.java
+++ app/src/main/java/com/example/petadoption/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.petadoption;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+public void goToSignUp(View view) {
+    Intent intent = new Intent(MainActivity.this, Signup.class);
+
+    startActivity(intent);
+}
 }
