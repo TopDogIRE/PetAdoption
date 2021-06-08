f2008d2915b5548196598ecb1bdf2064c918d82f
TopDogIRE <cathal777@gmail.com>
2021-06-07 21:24:12 -0700

    Added Post Page and button directing to it, connected activites together

--- app/src/main/java/com/example/petadoption/Adoptions.java
+++ app/src/main/java/com/example/petadoption/Adoptions.java
@@ -1,6 +1,8 @@
 package com.example.petadoption;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +13,12 @@ public class Adoptions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.adoptions);
-
-
     }
+
+    public void goToPost(View view) {
+        Intent intent = new Intent(this, NewPostActivity.class);
+        startActivity(intent);
+    }
+
 }
 
